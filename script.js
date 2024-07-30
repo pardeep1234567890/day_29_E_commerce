f095ecab927adc82a27cf33de21eea17d439f0df
pardeep1234567890 <pardeep905001@gmail.com>
2024-07-30 16:32:35 +0530

    make the app perfectly

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-        const apikey = "37668edf7a0aa47442cc8d27c77ca01e";
-        const apiurl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
-        const searchbox = document.querySelector(".search input")
-        const searchbtn = document.querySelector(".search button")
-        const weatherIcon = document.querySelector(".weather-icon")
+const apikey = "37668edf7a0aa47442cc8d27c77ca01e";
+const apiurl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+const searchbox = document.querySelector(".search input");
+const searchbtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
-        async function checkweather(city) {
-            const response = await fetch(apiurl + city + `&appid=${apikey}`);
-            if (response.status == 404) {
-                document.querySelector(".error").style.display = "block";
-                document.querySelector(".weather").style.display = "none";
-            }
-            else {
-                var data = await response.json();
+async function checkweather(city) {
+    try {
+        const response = await fetch(apiurl + city + `&appid=${apikey}`);
+        if (response.status == 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather").style.display = "none";
+        } else {
+            const data = await response.json();
 
-                document.querySelector(".city").innerHTML = data.name;
-                document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
-                document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-                document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
-                if (data.weather[0].main == "Clouds") {
-                    weatherIcon.src = "cloud.png"
-                }
-                else if (data.weather[0].main == "Clear") {
-                    weatherIcon.src = "sun.png"
-                }
-                else if (data.weather[0].main == "Rain") {
-                    weatherIcon.src = "rain.png"
-                }
-                else if (data.weather[0].main == "Drizzle") {
-                    weatherIcon.src = "drizzle.png"
-                }
-                else if (data.weather[0].main == "Mist") {
-                    weatherIcon.src = "mist.png"
-                }
-                else {
-                    weatherIcon.src = "https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png";
-                }
-                document.querySelector(".weather").style.display = "block";
-                document.querySelector(".error").style.display = "none";
+            document.querySelector(".city h2").innerHTML = data.name;
+            document.querySelector(".temp h1").innerHTML = Math.round(data.main.temp) + "°C";
+            document.querySelector(".hum-para p:first-child").innerHTML = data.main.humidity + "%";
+            document.querySelector(".para p:first-child").innerHTML = data.wind.speed + "km/h";
+
+            if (data.weather[0].main == "Clouds") {
+                weatherIcon.src = "cloud.png";
+            } else if (data.weather[0].main == "Clear") {
+                weatherIcon.src = "sun.png";
+            } else if (data.weather[0].main == "Rain") {
+                weatherIcon.src = "rain.png";
+            } else if (data.weather[0].main == "Drizzle") {
+                weatherIcon.src = "drizzle.png";
+            } else if (data.weather[0].main == "Mist") {
+                weatherIcon.src = "mist.png";
+            } else {
+                weatherIcon.src = "https://i.pinimg.com/originals/77/0b/80/770b805d5c99c7931366c2e84e88f251.png";
             }
 
+            document.querySelector(".weather").style.display = "block";
+            document.querySelector(".error").style.display = "none";
         }
-        searchbtn.addEventListener("click", () => {
-            checkweather(searchbox.value);
+    } 
+    catch (error) {
+        console.error("Error fetching weather data:", error);
+        document.querySelector(".error").style.display = "block";
+        document.querySelector(".weather").style.display = "none";
+    }
+}
 
-        })
+searchbtn.addEventListener("click", () => {
+    checkweather(searchbox.value);
+});
